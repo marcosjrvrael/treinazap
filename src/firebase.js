0ea4da79cd33d3a096e2b8379370ef9eee981b84
Marcos O Jr <39653959+marcosjrvrael@users.noreply.github.com>
2021-03-03 20:49:43 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';	
 
 const firebaseConfig = {	
-    apiKey: "AIzaSyAUNmXkl4U-iyMn-NMb6bAW8FKe1FSyV4Q",
-    authDomain: "zipzapson-14967.firebaseapp.com",
-    projectId: "zipzapson-14967",
-    storageBucket: "zipzapson-14967.appspot.com",
-    messagingSenderId: "114035079797",
-    appId: "1:114035079797:web:23cd52911de4e99f5ed63c"
+    apiKey: "${VAR_API}",
+    authDomain: "${VAR_AUTH}",
+    projectId: "${VAR_PROJECT}",
+    storageBucket: "${VAR_STORAGE}",
+    messagingSenderId: "${VAR_MESS}",
+    appId: "${VAR_APP}"
 };	
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);	
