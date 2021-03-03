4b8fc79ddbb8a25ff3bedafca62f68abb8dd3d74
Marcos O Jr <39653959+marcosjrvrael@users.noreply.github.com>
2021-03-02 21:22:14 -0300

    Update and rename firebase2.js to firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,12 @@
 import firebase from 'firebase';	
 
 const firebaseConfig = {	
-    apiKey: "API_KEY",
-    authDomain: "PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://PROJECT_ID.firebaseio.com",
-    projectId: "PROJECT_ID",
-    storageBucket: "PROJECT_ID.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID",
-    measurementId: "G-MEASUREMENT_ID",
+    apiKey: "AIzaSyAUNmXkl4U-iyMn-NMb6bAW8FKe1FSyV4Q",
+    authDomain: "zipzapson-14967.firebaseapp.com",
+    projectId: "zipzapson-14967",
+    storageBucket: "zipzapson-14967.appspot.com",
+    messagingSenderId: "114035079797",
+    appId: "1:114035079797:web:23cd52911de4e99f5ed63c"
 };	
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);	
